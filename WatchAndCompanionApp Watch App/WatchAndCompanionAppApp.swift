5224ce8b9ace72ece67b8954869e12cee7ef9f3c
MikeIsCooking <159172766+MikeeonTop@users.noreply.github.com>
2026-03-24 17:44:06 +0100

    Adding .gitignore file.

---
+++ WatchAndCompanionApp Watch App/WatchAndCompanionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchAndCompanionAppApp.swift
+//  WatchAndCompanionApp Watch App
+//
+//  Created by AFP PAL 21 on 25/03/26.
+//
+
+import SwiftUI
+
+@main
+struct WatchAndCompanionApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
