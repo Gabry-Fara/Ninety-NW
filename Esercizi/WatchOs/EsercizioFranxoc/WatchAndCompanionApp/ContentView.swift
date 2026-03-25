a8cf4bca0523a53ee31c2377a63aa6c4102117a5
MikeIsCooking <159172766+MikeeonTop@users.noreply.github.com>
2026-03-25 18:39:21 +0100

    aaaa
    aaaa

---
+++ Esercizi/WatchOs/EsercizioFranxoc/WatchAndCompanionApp/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  WatchAndCompanionApp
+//
+//  Created by AFP PAL 21 on 25/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+   var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Esercizi/WatchOs/EsercizioFranxoc/WatchAndCompanionApp/WatchAndCompanionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchAndCompanionAppApp.swift
+//  WatchAndCompanionApp
+//
+//  Created by AFP PAL 21 on 25/03/26.
+//
+
+import SwiftUI
+
+@main
+struct WatchAndCompanionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
