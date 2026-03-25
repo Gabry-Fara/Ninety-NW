c426c6db78ca4575b2984ec8fe9f79838a07f569
MikeIsCooking <159172766+MikeeonTop@users.noreply.github.com>
2026-03-25 17:25:37 +0100

    Revert "Merge remote-tracking branch 'refs/remotes/origin/main'"
    This reverts commit 327cc493dba461d918f7fd47b3bcf9e1abdbc051, reversing
    changes made to 69bd4c13cac1d01dfdd36762de4d0e0f0132877e.

---
+++ Esercizi/WatchOs/Schermate/Schermate Watch App/SchermateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchermateApp.swift
+//  Schermate Watch App
+//
+//  Created by AFP PAL 15 on 25/03/26.
+//
+
+import SwiftUI
+
+@main
+struct Schermate_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
