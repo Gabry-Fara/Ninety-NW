41a8505caf0d63303c50af715845378ac4294d90
MikeIsCooking <159172766+MikeeonTop@users.noreply.github.com>
2026-03-25 16:59:37 +0100

    aggiunta esercizio 1

---
+++ Esercizi/WatchOs/Schermate/Schermate/SchermateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchermateApp.swift
+//  Schermate
+//
+//  Created by AFP PAL 15 on 25/03/26.
+//
+
+import SwiftUI
+
+@main
+struct SchermateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
