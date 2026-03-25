8df4cf8374f1574a60073cdd85dbf26a7166cdb2
Gabry-Fara <gabriele.faraone.gf@gmail.com>
2026-03-25 16:29:49 +0100

    esercizi

---
+++ Esercizi/Tentativo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tentativo
+//
+//  Created by AFP PAL 15 on 25/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Esercizi/Tentativo/TentativoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TentativoApp.swift
+//  Tentativo
+//
+//  Created by AFP PAL 15 on 25/03/26.
+//
+
+import SwiftUI
+
+@main
+struct TentativoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
