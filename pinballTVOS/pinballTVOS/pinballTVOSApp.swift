e7946cafabccc4a66e2b3417dd75054282c74785
MikeIsCooking <159172766+MikeeonTop@users.noreply.github.com>
2026-03-26 16:23:12 +0100

    bdfsbssb

---
+++ pinballTVOS/pinballTVOS/pinballTVOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  pinballTVOSApp.swift
+//  pinballTVOS
+//
+//  Created by AFP PAL 21 on 26/03/26.
+//
+
+import SwiftUI
+
+@main
+struct pinballTVOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
