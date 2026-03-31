4e1fa95ca7218563598712db1298b73205da8c25
Gabry-Fara <gabriele.faraone.gf@gmail.com>
2026-03-31 14:34:41 +0200

    base di progetto

---
+++ UovoPasqua/UovoPasqua/UovoPasquaApp.swift
@@ -0,0 +1,17 @@
+//
+//  UovoPasquaApp.swift
+//  UovoPasqua
+//
+//  Created by AFP PAL 15 on 31/03/26.
+//
+
+import SwiftUI
+
+@main
+struct UovoPasquaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
