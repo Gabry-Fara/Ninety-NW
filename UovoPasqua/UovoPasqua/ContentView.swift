a6f0a7421faaf3c0aa280006ea76b54dbf23bfe3
MikeIsCooking <159172766+MikeeonTop@users.noreply.github.com>
2026-03-31 15:41:16 +0200

    uovo

--- UovoPasqua/UovoPasqua/ContentView.swift
+++ UovoPasqua/UovoPasqua/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Uovooooooo \n\n\n\n\n\n\n\n cartassassoforbici")
         }
         .padding()
     }
