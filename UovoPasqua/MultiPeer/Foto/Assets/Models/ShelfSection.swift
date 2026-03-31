d47849192cd6511b2e91ae0075fb9cff4ba0720e
Gabry-Fara <gabriele.faraone.gf@gmail.com>
2026-03-31 16:49:10 +0200

    asfd

--- UovoPasqua/MultiPeer/Foto/Assets/Models/ShelfSection.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-// a named row of media items used in home and library
-struct ShelfSection: Identifiable {
-    let id: String
-    let title: String
-    let subtitle: String?
-    let items: [MediaItem]
-}
