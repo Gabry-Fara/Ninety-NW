1eb34682fd648d4b914a54a057750cd8fe0c1ef1
Kuriix <70984123+Kuriix@users.noreply.github.com>
2026-04-02 17:23:40 +0200

    ninety core

---
+++ Ninety/NinetyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestfulNightApp.swift
+//  RestfulNight
+//
+//  Created by Deimante Valunaite on 07/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct NinetyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            TabBarView()
+        }
+    }
+}
